99834f310f6e5e69a1bdf9b88d944fcb61eb6d67
Egor <hoboman313@gmail.com>
2018-09-15 17:27:07 -0700

    lib upgrades, fix - don't fingerprint favicon files

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,9 +5,19 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     'ember-bootstrap': {
-      'bootstrapVersion': 4,
-      'importBootstrapFont': false,
-      'importBootstrapCSS': false
+      bootstrapVersion: 4,
+      importBootstrapFont: false,
+      importBootstrapCSS: false
+    },
+    fingerprint: {
+      exclude: [
+        'android-chrome-192x192.png',
+        'android-chrome-256x256.png',
+        'apple-touch-icon.png',
+        'favicon-16x16.png',
+        'favicon-32x32.png',
+        'mstile-150x150.png'
+      ]
     }
   });
 
